86a39b319592b600ed6141b776cca728c9ac3728
Yann Figueiredo <yann.fabricio@hotmail.com>
2020-12-30 09:45:16 -0300

    Criada classe do algoritmo do ponto médio e função de desenhar octante

---
+++ src/projetocg/PontoMedio.java
@@ -0,0 +1,9 @@
+package projetocg;
+
+import java.awt.Graphics;
+
+public class PontoMedio {
+    public void teste(int x, int y, int k, int z, Graphics g){
+        g.fillRect(x, y, k, z);
+    }
+}
