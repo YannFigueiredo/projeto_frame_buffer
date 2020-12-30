963828544a6fec448733d5cdbb1b53c07014b51a
Yann Figueiredo <yann.fabricio@hotmail.com>
2020-12-30 10:36:26 -0300

    Criada função para calcular pixels da borda de um círculo

--- src/projetocg/PontoMedio.java
+++ src/projetocg/PontoMedio.java
@@ -3,7 +3,39 @@ package projetocg;
 import java.awt.Graphics;
 
 public class PontoMedio {
-    public void teste(int x, int y, int k, int z, Graphics g){
-        g.fillRect(x, y, k, z);
+    int xCentral, yCentral, TAMPIXEL;
+    Graphics g;
+    
+    public void desenha8(int x, int y){
+        g.fillRect((x+xCentral)*TAMPIXEL, (y+yCentral)*TAMPIXEL, TAMPIXEL, TAMPIXEL);
+        g.fillRect((y+xCentral)*TAMPIXEL, (x+yCentral)*TAMPIXEL, TAMPIXEL, TAMPIXEL);
+        g.fillRect((y+xCentral)*TAMPIXEL, (-x+yCentral)*TAMPIXEL, TAMPIXEL, TAMPIXEL);
+        g.fillRect((x+xCentral)*TAMPIXEL, (-y+yCentral)*TAMPIXEL, TAMPIXEL, TAMPIXEL);
+        g.fillRect((-x+xCentral)*TAMPIXEL, (-y+yCentral)*TAMPIXEL, TAMPIXEL, TAMPIXEL);
+        g.fillRect((-y+xCentral)*TAMPIXEL, (-x+yCentral)*TAMPIXEL, TAMPIXEL, TAMPIXEL);
+        g.fillRect((-y+xCentral)*TAMPIXEL, (x+yCentral)*TAMPIXEL, TAMPIXEL, TAMPIXEL);
+        g.fillRect((-x+xCentral)*TAMPIXEL, (y+yCentral)*TAMPIXEL, TAMPIXEL, TAMPIXEL);
+    }
+    
+    public void desenharCirculo(int xCentral, int yCentral, int r, int TAMPIXEL, Graphics g){
+        g = g;
+        xCentral = xCentral;
+        yCentral = yCentral;
+        TAMPIXEL = TAMPIXEL;
+        
+        int x = 0;
+        int y = r;
+        int e = -r; //Erro
+        
+        desenha8(x ,y);
+        while(x<=y){
+            e = e+(2*x)+1;
+            x++;
+            if(e>=0){
+                e = e+2-(2*y);
+                y--;
+            }
+            desenha8(x, y);
+        }
     }
 }
